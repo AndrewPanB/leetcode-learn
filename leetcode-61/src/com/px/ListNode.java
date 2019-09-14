f346ed743c99704b85f063be966b5a1e67d35c63
Andrew Pan <px2557@foxmail.com>
2019-09-14 11:42:32 +0800

    Fix leetcode-61

--- leetcode-61/src/com/px/ListNode.java
+++ leetcode-61/src/com/px/ListNode.java
@@ -4,33 +4,36 @@ package com.px;
 public class ListNode {
     int val;
     ListNode next;
-    ListNode(int x) { val = x; }
+
+    ListNode(int x) {
+        val = x;
+    }
 }
 
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head==null || head.next ==null || k==0){
+        if (head == null || head.next == null || k == 0) {
             return head;
         }
         ListNode tempNode = head;
         ListNode tempNode2 = head;
         ListNode countNode = head;
-        int count =0;
-        while(countNode != null){
+        int count = 0;
+        while (countNode != null) {
             count++;
             countNode = countNode.next;
         }
-        count = k%count;
-        for(int i=0;i<count;i++){
-            if(tempNode!=null && tempNode.next!=null){
+        count = k % count;
+        for (int i = 0; i < count; i++) {
+            if (tempNode != null && tempNode.next != null) {
                 tempNode = tempNode.next;
-            }else if(tempNode!=null && tempNode.next==null){
+            } else if (tempNode != null && tempNode.next == null) {
                 tempNode = head;
-            }else{
+            } else {
                 return null;
             }
         }
-        while(tempNode!=null && tempNode.next!=null){
+        while (tempNode != null && tempNode.next != null) {
             tempNode = tempNode.next;
             tempNode2 = tempNode2.next;
         }
@@ -39,4 +42,163 @@ class Solution {
         tempNode2.next = null;
         return first;
     }
+B
+
+
+
+
+
+A
+A
+A
+
+C
+D
+A
+A
+A
+A
+A
+A
+A
+A
+A
+
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+
+A
+A
+A
+A
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
 }
